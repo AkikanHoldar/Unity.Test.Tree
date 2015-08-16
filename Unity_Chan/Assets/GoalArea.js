53f217ce9e35c48285da1736858bf39fd955611f
Akikan <akikanwg0@gmail.com>
2015-08-16 20:27:38 +0900

    AddUnityChanSource

---
+++ Unity_Chan/Assets/GoalArea.js
@@ -0,0 +1,11 @@
+﻿	static var myGoal:boolean;
+	static var myPose:boolean;
+	myGoal=false;                          
+	 
+	function OnTriggerEnter(myCol:Collider)
+	{
+	 if(myCol.tag=="Player")
+	 {
+  		myGoal=true;                                  
+	 }
+	}
---
+++ Unity_Chan/Assets/GoalText.js
@@ -0,0 +1,30 @@
+﻿static var time:float;
+
+function Start () {
+	time=0; 
+				  }
+	 
+	function Update () {
+	myGoal();                      
+	}
+	 
+	function myGoal()
+	{
+	 var msg=GameObject.Find("msg");
+ 	 var now:int=time;
+ 		 	 
+	if (GoalArea.myGoal==false)
+	 {
+	 	time+=Time.deltaTime;
+	 }else{
+	    msg.guiText.text="GOAL!"; 
+	    msg.guiText.fontSize=50;    
+	  	yield WaitForSeconds (2.0);
+	  	Application.LoadLevel(Application.loadedLevel);
+	  	GoalArea.myGoal=false;
+	 }
+
+	 guiText.text=now.ToString(); 
+
+	}
+	 	
